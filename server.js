020d8d4d8efc0c06dcf48764c4120e9d73ce7b1e
Jondo <chet9880@gmail.com>
2022-08-02 12:44:44 -0400

    updated server.js and installed npm packages

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
-const routes = require('./routes')
+
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+
+const routes = require('./controllers')
+const sequelize = require('./config/connection')
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+const sess = {
+    secret: 'Vince won\'t guess this seed',
+    resave: false,
+    saveUnitialized: false
+}
+
+app.use(session(sess))
+
+const hbs = exphbs.create({})
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
